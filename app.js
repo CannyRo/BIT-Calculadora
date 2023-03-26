0cb5249f68949d30ac23625f30a8eff5408523dc
CannyRo <whatcannydev@gmail.com>
2023-03-26 08:03:36 +0200

    rework of app.js to be more understandable, delete of virtual keyboard management, achive of real keyboard management

--- app.js
+++ app.js
@@ -3,54 +3,14 @@ console.log("Javascript is on")
 const app = {
 
     init : function(){
-        // INITIALISATION DES VARIABLES
         console.log("INIT")
-        app.value = 0
+        //INITIALISATION DES VARIABLES
+        app.value_first = 0;
+        app.value_second = 0;
+        app.value_result = 0;
+        app.operator = ""
 
-
-        // app.keybord = [
-        //     "zero" = "0",
-        //     "one" = "1",
-        //     "two" = "2",
-        //     "three" = "3",
-        //     "four" = "4",
-        //     "five" = "5",
-        //     "six" = "6",
-        //     "seven" = "7",
-        //     "eight" = "8",
-        //     "nine" = "9",
-        //     "dot" = ".",
-        //     "more" = "+",
-        //     "less" = "-",
-        //     "multiply" = "*",
-        //     "divide" = "/"
-        // ];
-        // ON RECUPERE LES TOUCHES DU CLAVIERS
-        //
-        // app.virtual_keyboard = [
-        //     app.zero = document.getElementById("zero"),
-        //     app.one = document.getElementById("one"),
-        //     app.two = document.getElementById("two"),
-        //     app.three = document.getElementById("three"),
-        //     app.four = document.getElementById("four"),
-        //     app.five = document.getElementById("five"),
-        //     app.six = document.getElementById("six"),
-        //     app.seven = document.getElementById("seven"),
-        //     app.eight = document.getElementById("eight"),
-        //     app.nine = document.getElementById("nine"),
-        //     app.dot = document.getElementById("dot"),
-        //     app.more = document.getElementById("more"),
-        //     app.less = document.getElementById("less"),
-        //     app.multiply = document.getElementById("multiply"),
-        //     app.divide = document.getElementById("divide"),
-        //     app.equal = document.getElementById("equal"),
-        //     app.back = document.getElementById("back"),
-        //     app.clear = document.getElementById("clear")
-        // ];
-        // console.log(app.virtual_keyboard);
-
-
-        // ON RECUPERE LES TOUCHES DU CLAVIERS
+        // ON RECUPERE LES TOUCHES DU CLAVIER VIRTUEL
         app.zero = document.getElementById("zero");
         app.one = document.getElementById("one");
         app.two = document.getElementById("two");
@@ -70,206 +30,173 @@ const app = {
         app.back = document.getElementById("back");
         app.clear = document.getElementById("clear");
 
-        // ON RECUPERE L'INPUT
+        // ON RECUPERE L'INPUT = L'ECRAN
         app.screen = document.getElementById("screen");
-        //console.log("app.screen");
-        //console.log(app.screen);
 
-        // ON LIT LE CLAVIER REEL
-        app.listen_real_keyboard();
-        
-        // ON LIT LE CLAVIER VIRTUEL
-        app.listen_virtual_keyboard();
+        // ON ECOUTE LE CLAVIER REEL ET ON AGIT
+        app.real_keyboard();
+
+        // ON ECOUTE LE CLAVIER VIRTUEL ET ON AGIT
+        app.virtual_keyboard();
     },
 
-    // ECOUTEURS DE LA SAISIE CLAVIER REEL
-    listen_real_keyboard : function(){
-        //console.log('RUN => listen_real_keyboard');
-        app.screen.onkeypress = (e) => {
-            // console.log("Event");
-            // console.log(e);
-            // console.log("KEYYYYYY");
-            // console.log(e.key);
-            // console.log(typeof(e.key))
-            let keypress = e.key;
-            // console.log("keypress : ")
-            // console.log(keypress)
-            if("0123456789.+-*/=".indexOf(keypress) < 0 ){
-                return false
-            }
-            app.read_and_update()
-        }
-        //app.screen.addEventListener("input", app.read_and_update);
+    // CONSOLE.LOG DES VARIABLES ===> 
+    //UTILE SIMPLEMENT POUR LE CONTRÔLE LORS DU DEVELOPEMENT
+    print_values : function(){
+        console.log(" ===///   PRINT   ///===");
+        console.log("app.value_first => ", app.value_first);
+        console.log(typeof(app.value_first));
+        console.log("app.value_second => ", app.value_second);
+        console.log(typeof(app.value_second));
+        console.log("app.value_result => ", app.value_result);
+        console.log(typeof(app.value_result));
+        console.log("app.operator => ", app.operator);
+        console.log(typeof(app.operator));
+        let input_current_value = app.get_value();
+        console.log("Input value =  ", input_current_value);
+        console.log(typeof(input_current_value));
+        console.log("===============");
     },
 
-    // ECOUTEURS DE LA SAISIE CLAVIER NUMERIQUE
-    // listen_virtual_keyboard : function(){
-    //     console.log('RUN => listen_virtual_keyboard');
-    //     app.virtual_keyboard.map( x => x.addEventListener("click", app.click_on_virtual_keyboard()))
-    //     // app.virtual_keyboard.map( x => console.log(x))
-    // },
-
-
-    // ECOUTEURS DE LA SAISIE CLAVIER NUMERIQUE
-    listen_virtual_keyboard : function(){
-        //console.log('RUN => listen_virtual_keyboard');
-        //
-        app.zero.addEventListener("click", (e)=>{ app.click_on_virtual_keyboard(e)} );
-        app.one.addEventListener("click", (e)=>{ app.click_on_virtual_keyboard(e)} );
-        app.two.addEventListener("click", (e)=>{ app.click_on_virtual_keyboard(e)} );
-        app.three.addEventListener("click", (e)=>{ app.click_on_virtual_keyboard(e)} );
-        app.four.addEventListener("click", (e)=>{ app.click_on_virtual_keyboard(e)} );
-        app.five.addEventListener("click", (e)=>{ app.click_on_virtual_keyboard(e)} );
-        app.six.addEventListener("click", (e)=>{ app.click_on_virtual_keyboard(e)} );
-        app.seven.addEventListener("click", (e)=>{ app.click_on_virtual_keyboard(e)} );
-        app.eight.addEventListener("click", (e)=>{ app.click_on_virtual_keyboard(e)} );
-        app.nine.addEventListener("click", (e)=>{ app.click_on_virtual_keyboard(e)} );
-        app.dot.addEventListener("click", (e)=>{ app.click_on_virtual_keyboard(e)} );
-        app.more.addEventListener("click", (e)=>{ app.click_on_virtual_keyboard(e)} );
-        app.less.addEventListener("click", (e)=>{ app.click_on_virtual_keyboard(e)} );
-        app.multiply.addEventListener("click", (e)=>{ app.click_on_virtual_keyboard(e)} );
-        app.divide.addEventListener("click", (e)=>{ app.click_on_virtual_keyboard(e)} );
-        app.equal.addEventListener("click", (e)=>{ app.click_on_virtual_keyboard(e)} );
-        app.back.addEventListener("click", (e)=>{ app.click_on_virtual_keyboard(e)} );
-        app.clear.addEventListener("click", (e)=>{ app.click_on_virtual_keyboard(e)} );
+    // ON RECUPERE LA VALEUR DE L'INPUT
+    get_value : function(){
+        let current_value = document.getElementById("screen").value;
+        console.log("current_value = ");
+        console.log(current_value);
+        return current_value;
     },
 
-    // DEFINITION DU CLICK SUR LE CLAVIER VIRTUEL
-    click_on_virtual_keyboard : function(e){
-        console.log(e);
-        console.log("ON A COMPRIS QU'IL Y A UN CLICK SUR : ",e.target.innerText);
-        let virtual_key_target = e.target.innerText;
-        console.log("E.TARGEEEEEEEEEEEEEEET");
-        console.log(e.target);
-        if(e.target.localName == "h2"){
-            console.log("t'as cliqué sur le caractère");
-            app.keypress_effect(e.target.parentNode);
-        } else {
-            app.keypress_effect(e.target);
-            
-        }
-        app.click_here(virtual_key_target);
-    },
-
-    // EFFET KEYPRESS... LE BOUTON S'ENFONCE QUAND ON CLIQUE DESSUS
-    keypress_effect : function(x){
-        // if(x.classList.contains("unpresseffect")){
-        //     x.setAttribute("class", "calculator_key presseffect");
-        // }
-        //setTimeout(x.setAttribute("class", "calculator_key updown"), 300);
-        //setTimeout(x.setAttribute("class", "calculator_key"), 300);
-
-        // setTimeout(x.classList.remove("presseffect"), 300);
-        // setTimeout(x.classList.remove("unpresseffect"), 300);
-        // x.removeAttribute("class");
-        // x.setAttribute("class", "calculator_key");
-        // console.log(x.classList);
-        // if(x.classList.contains("presseffect")){
-        //     x.classList.remove("presseffect");
-        // };
-        // if(x.classList.contains("unpresseffect")){
-        //     x.classList.remove("unpresseffect");
-        // };
-        //console.log("ON VA PRESSER LE BOUTON");
-        //console.log(x);
+    // ON APPUIE SUR UNE TOUCHE (VISUAL EFFECT)
+    press_effect : function(x){
         x.animate([
-            { transform: 'translateZ(0.5rem)'},
-            { transform: 'translateZ(0.125rem)', easing: "ease-in-out"},
-            { transform: 'translateZ(0.5rem)'}
+            { transform: "translateZ(0.5rem)"},
+            { transform: "translateZ(0.125rem)", easing: "ease-in-out"},
+            { transform: "translateZ(0.5rem)"}
             ], {
             duration: 300,
             iterations: 1
             })
-        // setTimeout(x.classList.add("presseffect"), 300);
-        // setTimeout(x.classList.add("unpresseffect"), 300);
     },
 
-    // SEQUENCE DE CLICK SUR LE CLAVIER VIRTUEL
-    click_here : function(x){
-        //console.log(x);
-        //console.log(typeof(x));
-        app.update_value_on_the_screen(x);
-    },
+    // ECOUTEUR DU CLAVIER REEL ET SEQUENCES
+    real_keyboard : function(){
+        console.log("Real Keyboard Listener activated");
+        // Contrôle et restriction des touches
+        app.screen.onkeypress = (e) => {
+            //console.log(e);
+            console.log(e.key);
+            let key_pressed = e.key;
+            
+            if("0123456789.+-*/=".indexOf(key_pressed) < 0 ){
+                return false;
+            }
+            if(key_pressed == "."){
+                console.log("POINT");
 
-    update_value_on_the_screen : function(x){
-        if(app.screen.value){
-            console.log("APP.SCREEN.VALUE => ",  app.screen.value);
-            if(x == "Clear"){
-                //local_value = "";
-                // console.log("LOCAL VALUE ==============> ");
-                // console.log(local_value);
-                // console.log("=======================")
-                app.screen.setAttribute("value", "");
-                app.screen.value = "";
+                let tempo_value = app.get_value();
+                console.log("tempo_value ==>  ", tempo_value);
+                
+                if(tempo_value.indexOf(key_pressed) >= 0){
+                    console.log("Il y a déja un POINT");
+                    return false;
+                }
                 return;
             }
-            if(x == "Back"){
-                console.log("BACK")
-                let new_value = app.screen.value.substring(0, app.screen.value.length-1);
-                // console.log("LOCAL VALUE ==============> ");
-                // console.log(local_value);
-                // console.log("=======================")
-                app.screen.setAttribute("value", new_value);
-                app.screen.value = new_value;
-                return;
+            if("+-*/".indexOf(key_pressed) >= 0){
+                console.log("OPERATEURS");
+                app.manage_operator(key_pressed);
+                return false;
             }
-            let tempo_value = app.screen.value + x;
-            app.screen.setAttribute("value", tempo_value);
-            app.screen.value = tempo_value;
-        } else {
-            if(x == "Clear"){
-                return;
+            if(key_pressed == "="){
+                console.log("EGAL... Lancer la séquence de calcul et donner le résultat");
+                app.value_second = app.get_value()*1;
+                app.calculate(app.operator);
+                return false;
             }
-            if(x == "Back"){
-                return;
-            }
-            console.log("APP.SCREEN => ", app.screen);
-            console.log("APP.SCREEN.VALUE => ",  app.screen.value);
-            app.screen.setAttribute("value", x);
-            app.screen.value=x;
-            // let new_attribute = document.createAttribute("value");
-            // new_attribute.value = x;
-            // app.screen.setAttributeNode(new_attribute.value);
+            
         }
-        // console.log('on peut deja lire la value car elle existe deja');
-        // console.log(app.screen.value);
-        // let new_attribute = document.createAttribute("value");
-        // new_attribute.value = x;
-        // app.screen.setAttributeNode(new_attribute.value);
     },
-
-    // MAJ DE LA VALEUR DE L'INPUT
-    read_and_update : function(){
-        console.log('RUN => read_screen');
-        // let value_first = app.screen.value;
-        let input_updated = document.createAttribute("value");
-        input_updated.value = app.screen.value;
-        app.screen.setAttributeNode(input_updated);
-        // app.value=value_first
-        console.log("VALUE UPDATED => ");
-        console.log(app.screen);
-        console.log(app.screen.value);
+    // ECOUTEUR DU CLAVIER VIRTUEL ET SEQUENCES
+    virtual_keyboard : function(){
+        console.log("Virtual Keyboard Listener activated");
     },
-
+    // ON GERE LA SAISIE D'UN OPERATEUR
+    manage_operator : function(x){
+        console.log("Manage Operator ON");
+        if(app.operator !== ""){
+            console.log("On a déjà un opérateur enregistré")
+            app.value_second = app.get_value()*1;
+            app.calculate(app.operator);
+        }
+        app.value_first = app.get_value()*1;
+        console.log("app.value_first ==>  ", app.value_first);
+        console.log(typeof(app.value_first));
+        switch(x){
+            case "+" : 
+                console.log("C'est un +");
+                app.operator = "+";
+                console.log("app.operator == >  ", app.operator);
+                app.screen.value = "";
+                break;
+            case "-" : 
+                console.log("C'est un -");
+                app.operator = "-";
+                console.log("app.operator == >  ", app.operator);
+                app.screen.value = "";
+                break;
+            case "*" : 
+                console.log("C'est un *");
+                app.operator = "*";
+                console.log("app.operator == >  ", app.operator);
+                app.screen.value = "";
+                break;
+            case "/" : 
+                console.log("C'est un /");
+                app.operator = "/";
+                console.log("app.operator == >  ", app.operator);
+                app.screen.value = "";
+                break;
+            default : 
+                console.log("NOT AN OPERATOR");
+                break;
+        }
+    },
+    // ON CALCULE LE RESULTAT (x = app.operator)
+    calculate : function(x){
+        console.log("Calculate");
+        switch(x){
+            case "+" : 
+                console.log("On additionne : ");
+                app.value_second = app.get_value()*1;
+                app.value_result = app.value_first + app.value_second;
+                app.screen.value = app.value_result;
+                break;
+            case "-" : 
+                console.log("On soustrait : ");
+                app.value_second = app.get_value()*1;
+                app.value_result = app.value_first - app.value_second;
+                app.screen.value = app.value_result;
+                break;
+            case "*" : 
+                console.log("On multiplie : ");
+                app.value_second = app.get_value()*1;
+                app.value_result = app.value_first * app.value_second;
+                app.screen.value = app.value_result;
+                break;
+            case "/" : 
+                console.log("On divise : ");
+                app.value_second = app.get_value()*1;
+                app.value_result = app.value_first / app.value_second;
+                app.screen.value = app.value_result;
+                break;
+            default : 
+                console.log("NOT AN OPERATOR");
+                app.value_result = app.get_value()*1;
+                app.screen.value = app.value_result;
+                break;
+        }
+        app.operator = "";
+    }
     
-
-    // document.getElementById("foo").onkeypress = function(e) {
-    //     var chr = String.fromCharCode(e.key);
-    //     if ("0123456789.+-*/=".indexOf(chr) < 0)
-    //         return false;
-    // };
-
-
-    // ECOUTEURS DU CLAVIER
-
-    // ECOUTEURS DE LA SOURIS (CLIC SUR LES TOUCHES VIRTUELS)
-
-
-
-
-
-
-
-
 }
 document.addEventListener("DOMContentLoaded", app.init)
